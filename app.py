55e5897353c60d24032f5a64c2e5e7c12391d9ca
SERGIO PARAMO <sergio.paramo@outlook.com>
2021-09-19 21:26:24 -0500

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 #Set app route
-app.route("/")
+@app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
